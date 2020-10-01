f8a39a7a4626374f54573775231d81f72e49b1aa
matthewxvoorhees <matthew.crnich@gmail.com>
2020-09-30 19:39:46 -0600

    Done.

---
+++ app/models/member.rb
@@ -0,0 +1,18 @@
+class Member
+
+        attr_accessor :name, :power, :biography
+    
+        @@all = []
+    
+        def initialize(args)
+            @name = args[:name]
+            @power = args[:power]
+            @biography = args[:biography]
+            @@all << self
+        end
+    
+        def self.all
+            @@all
+        end
+ 
+    end
